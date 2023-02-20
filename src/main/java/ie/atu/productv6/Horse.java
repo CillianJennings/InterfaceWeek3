30702e89bf8bc7341acf13ecf44efa04fbc5acd2
Cillian <G00404411@atu.ie>
2023-02-20 13:17:25 +0000

    Finished adding animal class and interface

---
+++ src/main/java/ie/atu/productv6/Horse.java
@@ -0,0 +1,47 @@
+package ie.atu.productv6;
+
+public class Horse extends Animal{
+
+    private String dam;
+    private String sire;
+    private double height;
+
+    public Horse() {
+        super();
+        dam = "";
+        sire = "";
+        height = 0;
+        count++;
+    }
+
+    public String getDam() {
+        return dam;
+    }
+
+    public void setDam(String dam) {
+        this.dam = dam;
+    }
+
+    public String getSire() {
+        return sire;
+    }
+
+    public void setSire(String sire) {
+        this.sire = sire;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + dam + '\n' +
+                "Sire: " + sire + '\n' +
+                "Height: " + height + 'm' + '\n';
+    }
+}
